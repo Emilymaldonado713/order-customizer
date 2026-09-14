34e60552f9d4e5cddefe3e97c9ccc3ca8ca7ee90
Emilymaldonado713 <emilymaldonado7137@gmail.com>
2026-09-14 17:26:00 +0000

    original student created add-on

--- src/data/menuItem.js
+++ src/data/menuItem.js
@@ -9,8 +9,7 @@ const menuItem = {
     { id: 'addon-cheese', label: 'Extra Cheddar', price: 1.25 },
     { id: 'addon-avocado', label: 'Avocado', price: 2.00 },
     { id: 'addon-jalapeno', label: 'Jalapeños', price: 0.75 },
-
-    { id: 'addon-onion', label: 'Crispy Onions', price: 1.00 },
+    { id: 'addon-fries', label: 'Fries', price: 2.50 },
 
   ],
 };
